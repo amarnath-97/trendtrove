32eea29d402fc39f3823d81c305c88bbacc97150
F22 Labs <amarnath@f22labs.com>
2023-04-18 19:18:57 +0530

    installed express and made the server running

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) =>{
+    res.send("Hello");
+});
+
+app.listen(8000, () =>{
+    console.log("Server is up and running...");
+});
